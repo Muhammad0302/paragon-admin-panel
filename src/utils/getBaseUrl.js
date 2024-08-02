4c1e7193823c4a2a2dd00d36d01baada006d4e3a
@mi477048 <mi477048@gmail.com>
2024-08-02 17:44:25 -0300

    changes added

--- src/utils/getBaseUrl.js
+++ src/utils/getBaseUrl.js
@@ -1,10 +1,10 @@
 export const getBaseUrl = () => {
-  return 'http://192.168.18.143/paragon-updated/api';
+  return 'http://thesfb.live/Autohub_Backend/api';
   // return 'http://192.168.18.15/Ismail_interior_Backend/api';
   // return 'https://all-start-driving-school-backend.vercel.app'
 };
 export const getBaseUrlSwati = () => {
-  return 'http://192.168.18.143/auto_hub_backend/api';
+  return 'http://thesfb.live/Autohub_Backend/api';
   // return 'http://192.168.18.15/Autohub_Backend/api';
   // return 'https://all-start-driving-school-backend.vercel.app'
 };
